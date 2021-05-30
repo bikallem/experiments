4ba6550f0d86bcc23e6469af6cccb28863110b49
Bikal Lem <gbikal@gmail.com>
2021-05-30 22:14:25 +0100

    add return

--- parser.ml
+++ parser.ml
@@ -3,23 +3,36 @@ module type IO = sig
 
   type channel
 
-  val read : channel -> bytes -> offset:int -> len:int -> int t
+  val return : 'a -> 'a t
+
+  val read : channel -> bytes -> len:int -> int t
 end
 
 module type P = sig
-  type 'a state
+  type 'a t
 
-  type ('a, 'b) t = 'a state -> 'b
+  type channel
+
+  type 'a io
+
+  val parse : channel -> 'a t -> 'a io
 end
 
-module Make (Io : IO) : P = struct
-  type 'a state =
-    { io : 'a Io.t
+module Make (Io : IO) :
+  P with type 'a io = 'a Io.t with type channel = Io.channel = struct
+  type channel = Io.channel
+
+  type 'a io = 'a Io.t
+
+  type state =
+    { io : channel
     ; buf : bytes
     ; mutable pos : int
     }
 
-  type ('a, 'b) t = 'a state -> 'b
+  type 'a t = state -> 'a
+
+  let parse (_channel : channel) (_p : 'a t) = assert false
 end
 
 module String_parser = Make (struct
@@ -27,8 +40,20 @@ module String_parser = Make (struct
 
   type channel = string
 
-  let read channel b ~offset ~len =
-    let s = String.sub channel offset len in
-    Bytes.blit_string s 0 b offset len;
+  let return a = a
+
+  let read channel b ~len =
+    let s = String.sub channel 0 len in
+    Bytes.blit_string s 0 b 0 len;
     len
+end)
+
+module Lwt_parser = Make (struct
+  type 'a t = 'a Lwt.t
+
+  type channel = Lwt_io.input_channel
+
+  let return = Lwt.return
+
+  let read channel bytes ~len = Lwt_io.read_into channel bytes 0 len
 end)
