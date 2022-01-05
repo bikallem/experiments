6aa2b184c3f9fabe2b1aa00d1fa54ca1a3a9ec3d
Bikal Lem <gbikal+git@gmail.com>
2022-01-05 10:57:02 +0000

    demonstrate eio issue

--- injective/injective.ml
+++ injective/injective.ml
@@ -1,56 +1,66 @@
 module type IO = sig
   type +'a t
+  type -'a u
 
-  val return : 'a -> 'a t
+  (* val return : 'a -> 'a t *)
 end
 
-module A : sig
-  type !'a t
+module A : IO = struct
+  type +'a t = 'a Eio.Std.Promise.t
 
-  val return : 'a -> 'a t
-end = struct
-  type !'a t = 'a
+  type -'a u = 'a Eio.Std.Promise.t
 
-  let return x : 'a t = x
+  (* let return x = *) 
 end
 
-module B : sig
-  type +!'a t
+(* module A : sig *)
+(*   type !'a t *)
 
-  val return : 'a -> 'a t
-end = struct
-  type +!'a t = 'a
+(*   val return : 'a -> 'a t *)
+(* end = struct *)
+(*   type !'a t = 'a *)
 
-  let return x : 'a t = x
-end
+(*   let return x : 'a t = x *)
+(* end *)
 
-module C : sig
-  type +'a t
+(* module B : sig *)
+(*   type +!'a t *)
 
-  val return : 'a -> 'a t
-end = struct
-  type +'a t = 'a
+(*   val return : 'a -> 'a t *)
+(* end = struct *)
+(*   type +!'a t = 'a *)
 
-  let return x = x
-end
+(*   let return x : 'a t = x *)
+(* end *)
 
-module Io_a : IO = struct
-  type +!'a t = 'a Atomic.t
+(* module C : sig *)
+(*   type +'a t *)
 
-  let return = A.return
-end
+(*   val return : 'a -> 'a t *)
+(* end = struct *)
+(*   type +'a t = 'a *)
 
-module Io_b : IO = struct
-  type +!'a t = 'a B.t
+(*   let return x = x *)
+(* end *)
 
-  let return = B.return
-end
+(* module Io_a : IO = struct *)
+(*   type +'a t = 'a Lwt.t *)
+(*   type 'a u = 'a Lwt.u *)
 
-module Io_c : IO = struct
-  type +'a t = 'a C.t
+(*   let return = Lwt.return *)
+(* end *)
 
-  let return = C.return
-end
+(* module Io_b : IO = struct *)
+(*   type +!'a t = 'a B.t *)
+
+(*   let return = B.return *)
+(* end *)
+
+(* module Io_c : IO = struct *)
+(*   type +'a t = 'a C.t *)
+
+(*   let return = C.return *)
+(* end *)
 
 let () =
   let (_ : string Io_b.t) = Io_b.return "hello" in
