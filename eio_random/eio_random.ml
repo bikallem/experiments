dce579a71fc981d136ba4b931d69522a3ca8c2c1
Bikal Lem <gbikal@gmail.com>
2022-04-29 12:05:41 +0100

    use mirage-crypto

--- eio_random/eio_random.ml
+++ eio_random/eio_random.ml
@@ -1,9 +1,5 @@
 let () =
-  (* Eio_luv.run @@ fun env -> *)
-  (* Eio_linux.run @@ fun env -> *)
   Eio_main.run @@ fun env ->
-  Eio.Switch.run @@ fun _sw ->
-  let randon_source = Eio.Stdenv.secure_random env in
-  let buf = Cstruct.create 32 in
-  let _got = Eio.Flow.read randon_source buf in
-  Printf.printf "Random number: %S%!" (Cstruct.to_string buf)
+  Mirage_crypto_rng_eio.run env @@ fun () -> 
+  let randon_num = Mirage_crypto_rng.generate 32 in 
+  Printf.printf "Random number: %S%!" (Cstruct.to_string randon_num)
